5758a05e2d8808e9473a81dbcc9afdbbd4a976e1
Martknap97 <knapskogmartine@gmail.com>
2020-02-21 14:29:13 +0100

    fikset test-mappen

---
+++ src/test/oblig1Prog/ValideringTest.java
@@ -0,0 +1,18 @@
+package oblig1Prog;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValideringTest {
+
+    @org.junit.jupiter.api.Test
+    void datoSjekk() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void epostSjekk() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void telefonSjekk() {
+    }
+}

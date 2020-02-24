b0d3e11593030b8b4479eb09e0eacab013c99d04
Frida Lines <frida.lines@gmail.com>
2020-02-24 11:13:40 +0100

    Update PersonFormatter.java

--- src/main/java/oblig1Prog/PersonFormatter.java
+++ src/main/java/oblig1Prog/PersonFormatter.java
@@ -3,17 +3,17 @@ package oblig1Prog;
 import java.util.List;
 
 public class PersonFormatter {
-    public static String DELIMITER = ";";
+    public static String DELIMITER = " - ";
 
-    public static String formatPerson(Person p){
-        return p.getNavn() + DELIMITER + p.getAlder() + DELIMITER + p.getFødselsdato()
-                + DELIMITER + p.getEpost() + DELIMITER + p.getTelefon();
+    public static String formatPerson(Person nyPerson){
+        return nyPerson.getNavn() + DELIMITER + nyPerson.getAlder() + DELIMITER + nyPerson.getFødselsdato()
+                + DELIMITER + nyPerson.getEpost() + DELIMITER + nyPerson.getTelefon();
     }
 
     public static String formatPerson (List<Person> personList){
         StringBuffer str = new StringBuffer();
-        for(Person p : personList){
-            str.append(formatPerson(p));
+        for(Person nyPerson : personList){
+            str.append(formatPerson(nyPerson));
             str.append("\n");
         }
         return str.toString();

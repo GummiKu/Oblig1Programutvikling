9ea6677b0b397a2609556867e52aaef72b2369b7
Martknap97 <knapskogmartine@gmail.com>
2020-02-24 13:25:05 +0100

    Fikset controller hent fra fil og lagre til fil

--- src/main/java/oblig1Prog/Controller.java
+++ src/main/java/oblig1Prog/Controller.java
@@ -10,6 +10,7 @@ import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Controller {
 
@@ -81,38 +82,49 @@ public class Controller {
 
     @FXML
     void hentFraFil(ActionEvent event) {
-        void hentFraFil(ActionEvent event) {
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle("Åpne fil");
-            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Kommaseparert verdi", "*.csv"));
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Tekstfil", "*.txt"));
             File fil = fileChooser.showOpenDialog(null);
             FileOpenerTxt leser = new FileOpenerTxt();
             try {
                 leser.lesFraFil(fil.getPath());
-            }catch (IOException e){
+            } catch (IOException e) {
                 lblError.setText(e.toString() + ": finner ikke fil.");
             }
 
-    }
+        }
 
-    @FXML
-    void lagreTilFil(ActionEvent event) {
+        @FXML
+        void lagreTilFil (ActionEvent event){
+            int alder = Integer.parseInt(txtAlder.getText());
+            int dag = Integer.parseInt(txtDag.getText());
+            int måned = Integer.parseInt(txtMåned.getText());
+            int år = Integer.parseInt(txtÅr.getText());
+            Dato fødselsdato = new Dato(dag, måned, år);
+            Person p = new Person(txtNavn.getText(), alder, fødselsdato, txtEpost.getText(), txtTelefon.getText());
+            var path = Paths.get("person.txt");
+            String formatert = PersonFormatter.formatPerson(p);
+            try {
+                FileSaverTxt.skrivFil(path, formatert);
+            } catch (IOException e) {
+                lblError.setText("Noe gikk feil: " + e.getMessage());
+            }
+        }
 
-    }
+        @FXML
+        void registrerPerson (ActionEvent event){
+            //Hente informasjon fra Label og legge til person i reg
 
-    @FXML
-    void registrerPerson(ActionEvent event) {
-        //Hente informasjon fra Label og legge til person i reg
-
-        int intAlder = Integer.parseInt(txtAlder.getText());
-        int intDag = Integer.parseInt(txtDag.getText());
-        int intMåned = Integer.parseInt(txtMåned.getText());
-        int intÅr = Integer.parseInt(txtÅr.getText());
-        Dato fødselsdato = new Dato(intDag, intMåned, intÅr);
-        Person enPerson = new Person(txtNavn.getText(), intAlder, fødselsdato, txtEpost.getText(), txtTelefon.getText());
-        reg.registrerPerson(enPerson);
+            int intAlder = Integer.parseInt(txtAlder.getText());
+            int intDag = Integer.parseInt(txtDag.getText());
+            int intMåned = Integer.parseInt(txtMåned.getText());
+            int intÅr = Integer.parseInt(txtÅr.getText());
+            Dato fødselsdato = new Dato(intDag, intMåned, intÅr);
+            Person enPerson = new Person(txtNavn.getText(), intAlder, fødselsdato, txtEpost.getText(), txtTelefon.getText());
+            reg.registrerPerson(enPerson);
 
 
-    }
+        }
 
 }
